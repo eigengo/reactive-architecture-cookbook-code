7dadb991fb9881d11a903a91c873289019d71c4d
Jan Machacek <jan.machacek@gmail.com>
2017-01-29 18:01:38 +0000

    Gen

--- dm-to-rms/recognition/test/main-test.cc
+++ dm-to-rms/recognition/test/main-test.cc
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <rapidcheck.h>
 #include <rapidcheck/gtest.h>
 #include "protobuf_gen.h"
 #include <envelope.pb.h>
@@ -9,21 +10,30 @@ using namespace com::reactivearchitecturecookbook;
 class main_test : public testing::Test {
 };
 
+RC_GTEST_FIXTURE_PROP(main_test, handle_extract_face, (const faceextract::v1m0::ExtractFace &gen)) {
+    const faceextract::v1m0::ExtractFace gen;
+    faceextract::v1m0::ExtractFace ser;
+    ser.ParseFromString(gen.SerializeAsString());
+
+    RC_ASSERT(ser.content() == gen.content());
+    RC_ASSERT(ser.mime_type() == gen.mime_type());
+
+    std::cout << "mime_type = " << gen.mime_type() << ", content = " << gen.content() << std::endl;
+}
+
+
 TEST_F(main_test, x) {
     faceextract::v1m0::ExtractFace extractFace;
+
     const google::protobuf::Descriptor *descriptor = extractFace.descriptor();
     for (int i = 0; i < descriptor->field_count(); ++i) {
         const google::protobuf::FieldDescriptor *fieldDescriptor = descriptor->field(i);
         switch (fieldDescriptor->cpp_type()) {
-            case google::protobuf::FieldDescriptor::CppType::CPPTYPE_STRING: break;
+            case google::protobuf::FieldDescriptor::CppType::CPPTYPE_STRING:
+                std::cout << fieldDescriptor->name() << std::endl;
+                break;
             default: break;
         }
     }
-}
 
-RC_GTEST_FIXTURE_PROP(main_test,
-                      copyOfStringIsIdenticalToOriginal,
-                      (const std::string &str)) {
-    const auto strCopy = str;
-    RC_ASSERT(strCopy == str);
 }
--- dm-to-rms/recognition/test/protobuf_gen.h
+++ dm-to-rms/recognition/test/protobuf_gen.h
@@ -1,19 +1,33 @@
 #ifndef ALL_PROTOBUF_GEN_H
 #define ALL_PROTOBUF_GEN_H
-#include <rapidcheck/Gen.hpp>
+
+#include <rapidcheck.h>
+#include <faceextract-v1m0.pb.h>
+
+using namespace com::reactivearchitecturecookbook;
 
 // NOTE: Must be in rc namespace!
 namespace rc {
 
-//    template<typename T>
-//    struct Arbitrary<T> {
-//        static Gen<T> arbitrary() {
-//            return gen::build<Person>(
-//                    gen::set(&Person::firstName),
-//                    gen::set(&Person::lastName),
-//                    gen::set(&Person::age, gen::inRange(0, 100)));
-//        };
-//    };
+    template<>
+    struct Arbitrary<faceextract::v1m0::ExtractFace> {
+        static Gen <faceextract::v1m0::ExtractFace> arbitrary() {
+            const std::vector<std::string> contentTypes {"image/png", "image/jpeg", "image/tiff", "image/bmp"};
+            const auto contentTypesGen2 = gen::map(gen::container<std::vector<char>>(32, gen::inRange<char>(32, 127)), [](const auto &chars) {
+                return std::string(chars.begin(), chars.end());
+            });
+            const auto contentTypesGen = gen::elementOf(contentTypes);
+
+            auto x = gen::tuple(contentTypesGen2, gen::arbitrary<std::string>());
+            return gen::map(x, [](const auto pair) {
+                faceextract::v1m0::ExtractFace extractFace;
+                extractFace.set_mime_type(std::get<0>(pair));
+                extractFace.set_content(std::get<1>(pair));
+                return extractFace;
+            });
+        };
+    };
+
 };
 
 
