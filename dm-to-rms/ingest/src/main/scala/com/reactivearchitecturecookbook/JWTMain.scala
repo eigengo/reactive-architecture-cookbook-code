7c76dfbae5eccd1c1849e4bea9a5aa1916a6210b
Jan Machacek <jan.machacek@gmail.com>
2017-01-31 18:10:17 +0000

    x

--- dm-to-rms/ingest/src/main/scala/com/reactivearchitecturecookbook/JWTMain.scala
+++ dm-to-rms/ingest/src/main/scala/com/reactivearchitecturecookbook/JWTMain.scala
@@ -3,5 +3,6 @@ package com.reactivearchitecturecookbook
 import com.nimbusds.jose._
 
 object JWTMain extends App {
-  new JWEObject(new JWEHeader(JWEAlgorithm.PBES2_HS512_A256KW, EncryptionMethod.A256CBC_HS512), new Payload("Hello, world"))
+  val jwe = new JWEObject(new JWEHeader(JWEAlgorithm.PBES2_HS512_A256KW, EncryptionMethod.A256CBC_HS512), new Payload("Hello, world"))
+  
 }
