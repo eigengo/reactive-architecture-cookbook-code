8a0a2f30f0d00917ce9356ca9cfd7b80b9e96adb
Jan Machacek <jan.machacek@gmail.com>
2017-02-02 15:14:12 +0000

    Tooling

--- tooling/tooling-docker.sh
+++ tooling/tooling-docker.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-cd native-devel
-docker build -t oreilly/rac-native-devel .
 
-cd ../native-runtime
+BASEDIR=$(dirname "$0")
+cd $BASEDIR/native-runtime
 docker build -t oreilly/rac-native-runtime .
+
+cd $BASEDIR/native-devel
+docker build -t oreilly/rac-native-devel .
