9a0540857e596206765f4386acb7663cebe15e65
Jan Machacek <jan.machacek@gmail.com>
2017-02-06 15:14:35 +0000

    Summary

---
+++ dm-to-rms/summary/build.sbt
@@ -0,0 +1,26 @@
+scalaVersion := "2.12.1"
+
+resolvers += Resolver.bintrayRepo("cakesolutions", "maven")
+
+lazy val akkaVersion = "2.4.16"
+lazy val scalaKafkaClientVersion = "0.10.1.2"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+  "net.cakesolutions" %% "scala-kafka-client" % scalaKafkaClientVersion,
+  "net.cakesolutions" %% "scala-kafka-client-akka" % scalaKafkaClientVersion,
+  "com.trueaccord.scalapb" %% "scalapb-json4s" % "0.1.6",
+  "com.trueaccord.scalapb" %% "scalapb-runtime" % com.trueaccord.scalapb.compiler.Version.scalapbVersion % "protobuf",
+  "com.nimbusds" % "nimbus-jose-jwt" % "4.34.1",
+  "org.scalatest" %% "scalatest" % "3.0.1" % "test",
+  "net.debasishg" %% "redisclient" % "3.3"
+)
+
+PB.includePaths in Compile ++= Seq(file("../protocol"))
+
+PB.protoSources in Compile := Seq(file("../protocol"))
+
+PB.targets in Compile := Seq(
+  scalapb.gen(flatPackage = true) -> (sourceManaged in Compile).value
+)
--- tooling/build.sh
+++ tooling/build.sh
@@ -2,25 +2,27 @@
 
 BASEDIR="$(cd "$(dirname "$0")" && pwd)"
 
-cd $BASEDIR/native-devel
-docker build -t oreilly/rac-native-devel .
-
-cd $BASEDIR/native-runtime
-docker build -t oreilly/rac-native-runtime .
+# cd $BASEDIR/native-devel
+# docker build -t oreilly/rac-native-devel .
+#
+# cd $BASEDIR/native-runtime
+# docker build -t oreilly/rac-native-runtime .
 
 BASEDIR_BIN=$BASEDIR/.bin
 DOCKER_SQUASH=$BASEDIR_BIN/docker-squash
-if [ ! -f $DOCKER_SQUASH ]; then
-  DOCKER_SQUASH_URL=""
-  if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    DOCKER_SQUASH_URL="https://github.com/jwilder/docker-squash/releases/download/v0.2.0/docker-squash-linux-amd64-v0.2.0.tar.gz"
-  elif [[ "$OSTYPE" == "darwin"* ]]; then
-    DOCKER_SQUASH_URL="https://github.com/jwilder/docker-squash/releases/download/v0.2.0/docker-squash-darwin-amd64-v0.2.0.tar.gz"
-  else
-    echo "Don't know how to stip outside mac OS or Linux."
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  DOCKER_SQUASH_URL="https://github.com/jwilder/docker-squash/releases/download/v0.2.0/docker-squash-linux-amd64-v0.2.0.tar.gz"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  PATH="/usr/local/opt/gnu-tar/libexec/gnubin":$PATH
+  if [[ `tar --version` == "bsdtar"* ]]; then
+    echo "This script needs gnu-tar to work."
   fi
-  mkdir -p $BASEDIR_BIN
-  curl $DOCKER_SQUASH_URL -L -o $BASEDIR_BIN/x.tgz && tar xzf x.tgz && rm x.tgz
+  DOCKER_SQUASH_URL="https://github.com/jwilder/docker-squash/releases/download/v0.2.0/docker-squash-darwin-amd64-v0.2.0.tar.gz"
+else
+  echo "Don't know how to stip outside mac OS or Linux."
 fi
-
-docker save `docker images oreilly/rac-native-runtime:latest -qa` | $BASEDIR_BIN/docker-squash -t squash -verbose | docker load
+if [ ! -f $DOCKER_SQUASH ]; then
+  mkdir -p $BASEDIR_BIN
+  curl $DOCKER_SQUASH_URL -L -o $BASEDIR_BIN/x.tgz && tar xzf $BASEDIR_BIN/x.tgz && rm $BASEDIR_BIN/x.tgz
+fi
+docker save `docker images oreilly/rac-native-runtime:latest -qa` | sudo $BASEDIR_BIN/docker-squash -t squash -verbose | docker load
