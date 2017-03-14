fb7ccf962ba0d7996c7aac49a32db7e6552c8b44
Jan Machacek <jan.machacek@gmail.com>
2017-03-14 10:03:10 +0000

    x

---
+++ eas/ingest/src/github.com/reactivesystemsarchitecture/eas/ingest/handlers_test.go
@@ -0,0 +1,25 @@
+package ingest
+
+import (
+	"testing"
+	"github.com/reactivesystemsarchitecture/eas/protocol"
+	"errors"
+)
+
+var f = EnvelopeHandlerFunc(func(_ *protocol.Envelope) error {
+	return errors.New("bab")
+})
+
+func TestEnvelopeHandlerFunc_Handle(t *testing.T) {
+	var e protocol.Envelope
+	if f.Handle(&e).Error() != "bab" {
+		t.Fail()
+	}
+}
+
+func TestEnvelopeHandlerFunc_Validate(t *testing.T) {
+	var e protocol.Envelope
+	if f.Validate(&e) != nil {
+		t.Fail()
+	}
+}
