7cf379a0f5d70ec5d8297644521397f93af105df
Jan Machacek <jan.machacek@gmail.com>
2017-03-19 15:26:48 +0000

    iOS

---
+++ eas/ios/EAS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EAS
+//
+//  Created by Jan Machacek on 19/03/2017.
+//  Copyright © 2017 Jan Machacek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ eas/ios/protocol/protocol/protocol.h
@@ -0,0 +1,19 @@
+//
+//  protocol.h
+//  protocol
+//
+//  Created by Jan Machacek on 19/03/2017.
+//  Copyright © 2017 Jan Machacek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for protocol.
+FOUNDATION_EXPORT double protocolVersionNumber;
+
+//! Project version string for protocol.
+FOUNDATION_EXPORT const unsigned char protocolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <protocol/PublicHeader.h>
+
+
